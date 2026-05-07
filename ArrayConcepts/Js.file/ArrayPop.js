8a51d28c1b38d59a64a8d85bec9b6048445842db
likithadavala <likithadavala99@gmail.com>
2026-05-07 16:17:57 +0530

    ArrayPop

---
+++ ArrayConcepts/Js.file/ArrayPop.js
@@ -0,0 +1,35 @@
+var animals = [];
+var content = "";
+function onClickPush(){
+    debugger;
+    var textBox = document.getElementById("txtTextBox").value;
+    animals.push(textBox);
+    for (i = 0; i < animals.length; i++)
+        content = content + "<p>" + (i + 1) + "." + animals[i] + "</p><br>"
+    document.getElementById("pResult").innerHTML = content;
+    content = "";
+}
+function onClickPop(){
+    animals.pop();
+    for (i = 0; i < animals.length; i++)
+        content = content + "<p>" + (i + 1) + "." + animals[i] + "</p><br>"
+    document.getElementById("pResult").innerHTML = content;
+    content = "";
+}
+function onClickShift(){
+    debugger;
+    animals.shift();
+    for (i = 0; i < animals.length; i++)
+        content = content + "<p>" + (i + 1) + "." + animals[i] + "</p><br>"
+    document.getElementById("pResult").innerHTML = content;
+    content = "";
+}
+function onClickUnshift(){
+    debugger;
+    var textBox = document.getElementById("txtTextBox").value;
+    animals.unshift(textBox);
+    for (i = 0; i < animals.length; i++)
+        content = content + "<p>" + (i + 1) + "." + animals[i] + "</p><br>"
+    document.getElementById("pResult").innerHTML = content;
+    content = "";
+}
